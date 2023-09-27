fa6436136d01f45f8fa0226d7eba801789ff4cdb
Umut Özbilek <umutozbilek@hotmail.com>
2023-09-27 21:45:54 +0300

    son hal

--- ProjectWebApi/Program.cs
+++ ProjectWebApi/Program.cs
@@ -1,4 +1,16 @@
 
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Business.DependencyResolvers.Autofac;
+using Autofac.Core;
+using Core.DependencyResolvers;
+using Core.Utilities.IoC;
+using Core.Extensions;
+using Core.Utilities.Security.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Core.Utilities.Security.Encryption;
+
 namespace ProjectWebApi
 {
     public class Program
@@ -6,13 +18,41 @@ namespace ProjectWebApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+            // Register services directly with Autofac here.
+            // Don't call builder.Populate(), that happens in AutofacServiceProviderFactory.
+            builder.Host.ConfigureContainer<ContainerBuilder>(
+               builder => builder.RegisterModule(new AutofacBusinessModule()));
+
+            builder.Services.AddDependencyResolvers(new ICoreModule[] {
+                new CoreModule()
+            });
+            builder.Services.AddCors();
+
+            var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+           builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = tokenOptions.Issuer,
+                        ValidAudience = tokenOptions.Audience,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+                    };
+                });
 
             var app = builder.Build();
 
@@ -22,6 +62,7 @@ namespace ProjectWebApi
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
